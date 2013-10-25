1c6e1208c789efd73b2638eb758f8869795aac2e
Elliott Murray <emurray@dius.com.au>
2013-10-25 15:49:49 +1100

    deleted observers

--- app/models/retro_observer.rb
+++
@@ -1,7 +0,0 @@
-class RetroObserver < Mongoid::Observer
-  def after_create retro
-    # retro.retro_reflections << RetroReflection.new(title: 'Went well')
-    # retro.retro_reflections << RetroReflection.new(title: 'Not so well')
-    # retro.retro_reflections << RetroReflection.new(title: 'Ideas')
-  end
-end
